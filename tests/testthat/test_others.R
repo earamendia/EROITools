0435aa74e641901e3d47aca9ca0a3c76ed2f880e
earamendia <eeear@leeds.ac.uk>
2022-05-10 15:39:12 +0100

    Testing infrastructure fully set up

---
+++ tests/testthat/test_others.R
@@ -0,0 +1,45 @@
+
+# test_that("calc_fec_from_ff_by_group",{
+#   
+#   
+#   
+# })
+
+
+
+# test_that("calc_fec_from_ff_as_fuel_by_group",{
+#   
+#   
+#   
+# })
+
+
+
+# test_that("calc_fec_from_ff_as_elec_by_group",{
+#   
+#   
+#   
+# })
+
+
+# test_that("calc_share_elec_supply_by_ff_group",{
+#   
+#   
+#   
+# })
+
+
+# test_that("calc_fec_from_ff_as_heat_by_group",{
+#   
+#   
+#   
+# })
+
+
+# test_that("calc_share_heat_supply_by_ff_group",{
+#   
+#   
+#   
+# })
+
+
---
+++ tests/testthat/test_shares.R
@@ -0,0 +1,37 @@
+
+# test_that("calc_share_primary_ff_supply_by_product_by_group works",{
+#   
+#   
+#   
+# })
+
+
+# test_that("calc_share_ff_use_by_product_by_group works",{
+#   
+#   
+#   
+# })
+
+
+
+# test_that("calc_shares_elec_by_ff_group works",{
+#   
+#   
+#   
+# })
+
+
+
+# test_that("calc_shares_heat_by_ff_group works",{
+#   
+#   
+#   
+# })
+
+
+
+# test_that("calc_shares_ff_by_group_inc_elec_heat works",{
+#   
+#   
+#   
+# })
