16085e7d11a241ef6a4de119f40fb288ba4ce3db
earamendia <eeear@leeds.ac.uk>
2022-05-16 17:11:53 +0100

    Structure of calc_avg_efficiency_by_ff_group() function writen. Still finish the actual avg efficiency calculations and write tests.

--- R/useful_stage.R
+++ R/useful_stage.R
@@ -67,3 +67,163 @@ push_to_useful_erois <- function(.tidy_io_erois,
   
   return(tidy_useful_erois)
 }
+
+
+
+#' Title
+#'
+#' @param .tidy_efficiencies_df 
+#' @param .tidy_iea_df 
+#' @param include_non_energy_uses 
+#' @param final_use_mats 
+#' @param list_primary_oil_products 
+#' @param list_primary_coal_products 
+#' @param list_primary_gas_products 
+#' @param list_oil_products 
+#' @param list_coal_products 
+#' @param list_gas_products 
+#' @param product.group 
+#' @param country 
+#' @param method 
+#' @param energy_type 
+#' @param last_stage 
+#' @param year 
+#' @param product 
+#' @param non_energy_uses 
+#' @param eroi.method 
+#' @param type 
+#' @param boundary 
+#' @param share 
+#' @param useful_stage_eroi 
+#' @param group.eroi 
+#' @param energy.stage 
+#' @param product_without_origin 
+#' @param calc_method 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_avg_efficiency_by_ff_group <- function(.tidy_efficiencies_df,
+                                            .tidy_iea_df,
+                                            # Whether you want to include non-energy uses products in the EROI calculation
+                                            include_non_energy_uses = FALSE,
+                                            # Which matrices flows to use for calculating shares
+                                            final_use_mats = c(IEATools::psut_cols$Y, IEATools::psut_cols$U_eiou),
+                                            # Lists defining each product group
+                                            list_primary_oil_products = IEATools::primary_oil_products,
+                                            list_primary_coal_products = IEATools::primary_coal_products,
+                                            list_primary_gas_products = IEATools::primary_gas_products,
+                                            list_oil_products = IEATools::oil_and_oil_products,
+                                            list_coal_products = IEATools::coal_and_coal_products,
+                                            list_gas_products = IEATools::primary_gas_products,
+                                            # Do not change
+                                            product.group = "Product.Group",
+                                            country = IEATools::iea_cols$country,
+                                            method = IEATools::iea_cols$method,
+                                            energy_type = IEATools::iea_cols$energy_type,
+                                            last_stage = IEATools::iea_cols$last_stage,
+                                            year = IEATools::iea_cols$year,
+                                            product = IEATools::iea_cols$product,
+                                            non_energy_uses = "Non_Energy_Uses",
+                                            eroi.method = "Eroi.method",
+                                            type = "Type",
+                                            boundary = "Boundary",
+                                            share = "Share",
+                                            useful_stage_eroi = "Useful_Stage_EROI",
+                                            group.eroi = "Group.eroi",
+                                            energy.stage = "Energy.stage",
+                                            product_without_origin = "product_without_origin",
+                                            calc_method = c("dta", "gma")){
+  
+  ### Preparing the .tidy_iea_df so that it has a new "product_without_origin" column,
+  # which will be equal to "product" when we are not using a MR-PSUT framework
+  cols_to_check <- c(product_without_origin = NA_character_)
+  
+  .tidy_iea_df <- .tidy_iea_df %>%
+    tibble::add_column(!!!cols_to_check[!names(cols_to_check) %in% names(.)]) %>%
+    dplyr::mutate(
+      "{product_without_origin}" := dplyr::case_when(
+        is.na(.data[[product_without_origin]]) ~ .data[[product]],
+        TRUE ~ .data[[product_without_origin]]
+      )
+    )
+  
+  # Pulling out the list of countries contained in .tidy_iea_df
+  list_countries_in_tidy_df <- .tidy_iea_df %>%
+    dplyr::ungroup() %>%
+    dplyr::select(.data[[country]]) %>%
+    dplyr::distinct() %>%
+    dplyr::pull()
+  
+  
+  ### Big first step - Building the tidy shares data frame ###
+  
+  # (1) Calculating tidy shares by product at the final (fuel) stage:
+  tidy_shares_final_fuel_df <- calc_share_ff_use_by_product_by_group(.tidy_iea_df,
+                                                                     include_non_energy_uses = include_non_energy_uses,
+                                                                     final_use_mats = final_use_mats,
+                                                                     list_oil_products = list_oil_products,
+                                                                     list_coal_products = list_coal_products,
+                                                                     list_gas_products = list_gas_products) %>%
+    dplyr::mutate(
+      "{energy.stage}" := "Useful (fuel)"
+    )
+  
+  # (2) Calculating tidy shares by product at the final (elec) stage:
+  # THIS CHUNK SHOULD BE SQUEEZED IN THE calc_shares_elec_by_ff_group() FUNCTION
+  # BUT WE NEED TESTS TO DO THAT...
+  tidy_shares_final_elec_df <- calc_shares_elec_by_ff_group(.tidy_iea_df) %>% 
+    dplyr::mutate(
+      "{energy.stage}" := "Useful (electricity)"
+    )
+  
+  # (3) Calculating tidy shares by product at the final (heat) stage:
+  # THIS CHUNK SHOULD BE SQUEEZED IN THE calc_shares_heat_by_ff_group() FUNCTION
+  # BUT WE NEED TESTS TO DO THAT...
+  tidy_shares_final_heat_df <- calc_shares_heat_by_ff_group(.tidy_iea_df) %>% 
+    dplyr::mutate(
+      "{energy.stage}" := "Useful (heat)"
+    )
+  
+  # (4) Calculating tidy shares by product at the final (fuel+elec+heat) stage:
+  tidy_shares_ff_by_group_inc_elec_heat <- calc_shares_ff_by_group_inc_elec_heat(.tidy_iea_df,
+                                                                                 include_non_energy_uses = include_non_energy_uses,
+                                                                                 final_use_mats = final_use_mats,
+                                                                                 list_oil_products = list_oil_products,
+                                                                                 list_coal_products = list_coal_products,
+                                                                                 list_gas_products = list_gas_products) %>%
+    dplyr::mutate(
+      "{energy.stage}" := "Useful (fuel+elec+heat)"
+    )
+  
+  # (5) Merging all shares together:
+  tidy_shares_df <- dplyr::bind_rows(
+    tidy_shares_final_fuel_df,
+    tidy_shares_final_elec_df,
+    tidy_shares_final_heat_df,
+    tidy_shares_ff_by_group_inc_elec_heat
+  )
+  
+  
+  ### Big second step - Determining average FU efficiencies from there ###
+  
+  if (calc_method == "dta"){
+    
+    average_FU_efficiencies <- 1
+    
+    
+  } else if (calc_method == "gma"){
+    
+    average_FU_efficiencies <- 2
+    
+  }
+  
+  return(average_FU_efficiencies)
+}
+
+
+
+
+
+
--- tests/testthat/test_useful_stage.R
+++ tests/testthat/test_useful_stage.R
@@ -1,8 +1,130 @@
 
 test_that("push_to_useful_erois works",{
   
+  # Path to dummy AB data
+  A_B_path <- system.file("extdata/A_B_data_full_2018_format_stat_diffs_stock_changes.csv", package = "EROITools")
+  
+  # Loading data
+  tidy_AB_data <- A_B_path %>%
+    IEATools::load_tidy_iea_df() %>%
+    IEATools::specify_all() %>%
+    ECCTools::specify_elect_heat_renewables() %>%
+    ECCTools::specify_elect_heat_fossil_fuels() %>%
+    ECCTools::specify_elect_heat_nuclear() %>%
+    ECCTools::specify_other_elec_heat_production() %>%
+    ECCTools::specify_elect_heat_markets() %>%
+    IEATools::add_psut_matnames() %>%
+    ECCTools::stat_diffs_to_balancing() %>%
+    ECCTools::stock_changes_to_balancing()
+  
+  
+  # FIRST, WE TEST THE DTA APPROACH
+  
+  # Calculating total use of each product
+  tidy_AB_dta <- tidy_AB_data %>%
+    ECCTools::transform_to_dta(requirement_matrices_list = c("U_feed"),
+                               select_dta_observations = FALSE)
+  
+  # Calculating IO matrices
+  tidy_io_AB_dta <- tidy_AB_data %>%
+    IEATools::prep_psut() %>%
+    Recca::calc_io_mats(method_q_calculation = "sum_R_V_cols")
+  
+  # Calculating tidy IO EROIs
+  tidy_AB_erois_dta <- tidy_io_AB_dta %>%
+    Recca::calc_E_EIOU() %>%
+    Recca::calc_erois() %>%
+    EROITools::extract_tidy_product_erois() %>%
+    dplyr::mutate(
+      Eroi.method = "DTA"
+    ) %>%
+    dplyr::relocate(.data[["Eroi.method"]], .after = Year)
+  
+  # Pushing to tidy useful stage EROIs
+  length_to_use <- tidy_AB_erois_dta %>% 
+    dplyr::select(Country, Method, Energy.type, Year, Product) %>% 
+    dplyr::distinct() %>% 
+    nrow()
+  
+  tidy_FU_efficiencies_dta <- tidy_AB_erois_dta %>% 
+    dplyr::select(Country, Method, Energy.type, Year, Product) %>% 
+    dplyr::distinct() %>% 
+    dplyr::mutate(
+      Average_Efficiency_Global = seq(0.15, 1, 0.85/(length_to_use-1))
+    )
+  
+  tidy_useful_erois_dta <- tidy_AB_erois_dta %>% 
+    dplyr::left_join(tidy_FU_efficiencies_dta,
+                     by = c("Country", "Method", "Energy.type", "Year", "Product")) %>%
+    dplyr::mutate(
+      Useful_Stage_EROI = Average_Efficiency_Global * EROI
+    ) %>% 
+    dplyr::filter(! is.na(Useful_Stage_EROI))
+  
+  tidy_useful_erois_dta_with_function <- push_to_useful_erois(
+    .tidy_io_erois = tidy_AB_erois_dta,
+    tidy_FU_efficiencies = tidy_FU_efficiencies_dta,
+    eroi_calc_method = "dta"
+  )
   
   
   
   
+  
+  
+  
+  
+  
+  
+  
+  # SECOND, WE TEST THE GMA APPROACH
+  
+  tidy_AB_data_gma <- tidy_AB_data %>%
+    ECCTools::transform_to_gma()
+  
+  # Calculates IO mats - GMA
+  tidy_io_AB_gma <- tidy_AB_data_gma %>% 
+    IEATools::prep_psut() %>% 
+    Recca::calc_io_mats(method_q_calculation = "sum_R_V_cols")
+  
+  # Calculates tidy IO erois - GMA
+  tidy_AB_erois_gma <- tidy_io_AB_gma %>% 
+    Recca::calc_E_EIOU() %>% 
+    Recca::calc_erois() %>% 
+    EROITools::extract_tidy_product_erois() %>% 
+    dplyr::mutate(
+      Eroi.method = "DTA"
+    ) %>% 
+    dplyr::relocate(.data[["Eroi.method"]], .after = Year)
+  
+  # Prepare GMA data frame for shares calculations
+  # tidy_AB_data_gma_prepared <- tidy_AB_data_gma %>% 
+  #   prepare_gma_for_shares()
+  
+  # Pushing to tidy useful stage EROIs
+  length_to_use <- tidy_AB_erois_gma %>% 
+    dplyr::select(Country, Method, Energy.type, Year, Product) %>% 
+    dplyr::distinct() %>% 
+    nrow()
+  
+  tidy_FU_efficiencies_gma <- tidy_AB_erois_gma %>% 
+    dplyr::select(Country, Method, Energy.type, Year, Product) %>% 
+    dplyr::distinct() %>% 
+    dplyr::mutate(
+      Average_Efficiency_Global = seq(0.15, 1, 0.85/(length_to_use-1))
+    )
+  
+  tidy_useful_erois_gma <- tidy_AB_erois_gma %>% 
+    dplyr::left_join(tidy_FU_efficiencies_gma,
+                     by = c("Country", "Method", "Energy.type", "Year", "Product")) %>%
+    dplyr::mutate(
+      Useful_Stage_EROI = Average_Efficiency_Global * EROI
+    ) %>% 
+    dplyr::filter(! is.na(Useful_Stage_EROI))
+  
+  tidy_useful_erois_gma_with_function <- push_to_useful_erois(
+    .tidy_io_erois = tidy_AB_erois_gma,
+    tidy_FU_efficiencies = tidy_FU_efficiencies_gma,
+    eroi_calc_method = "gma"
+  )
 })
